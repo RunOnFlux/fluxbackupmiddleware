8d97b46943e1d72b81fb6bf2f47767d726d45a36
Ali Mahdavi <alihmahdavi@gmail.com>
2026-06-30 16:56:19 +0200

    increase user quota

--- config/default.js
+++ config/default.js
@@ -6,8 +6,8 @@ module.exports = {
   dbPort: 3306,
   dbhost: '127.0.0.1',
   maxConcurrentTasks: 10,
-  quotaPerUser: 10, // GB
-  maxFilesPerApp: 12,
+  quotaPerUser: 20, // GB
+  maxFilesPerApp: 20,
   storagePath: './tmp/',
   hostAPIPath: '/',
   fluxTeamZelId: '1hjy4bCYBJr4mny4zCE85J94RXa8W6q37',
